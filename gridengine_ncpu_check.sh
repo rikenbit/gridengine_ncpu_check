61894fe21bbec13abf90e222eaba59d8ca867331
Manabu ISHII <manabu.ishii.rb@gmail.com>
2017-10-17 17:26:15 +0900

    Ignore some lines

--- gridengine_ncpu_check.sh
+++ gridengine_ncpu_check.sh
@@ -2,15 +2,18 @@
 declare -A TOTAL
 TOTAL=()
 qhost -j | sed -e "\$a END" | tail -n +3 |  grep -v ^"\s"*job | grep -v ^"\s"*------ |  while read LINE;
-do 
+do
   HOST=$(echo ${LINE} | awk '{print $1;}' | grep  -v ^'[0-9]')
   RET=$?
-  if [ ${RET} -eq 0  ]; then
-    for i in ${!TOTAL[@]}; do
-      echo "[$i]=>[${TOTAL[$i]}] NCPU"
-    done
-    echo 
-    echo "$LINE"
+  if [ ${RET} -eq 0 ]; then
+    CHECK=$(echo "$LINE" | awk '{print $8;}')
+    if [  -n "${CHECK}" -o "END" = "$LINE" ]; then
+      for i in ${!TOTAL[@]}; do
+        echo "[$i]=>[${TOTAL[$i]}] NCPU"
+      done
+      echo
+      echo "$LINE"
+    fi
     TOTAL=()
   else
     ACCOUNT=$(echo ${LINE} | awk '{print $4;}')
@@ -19,10 +22,11 @@ do
     RETNCPUPART=$?
     NCPU=1
     if [ ${RETNCPUPART} -eq 0  ]; then
-      NCPU="MUSTBEEXTRACT"  
+      NCPU="MUSTBEEXTRACT"
       NCPU=$(qstat -j ${JOBID} | grep ^'hard resource_list' |grep ncpu|sed 's/.*ncpu=\([0-9]*\)/\1/g')
     fi
     TOTAL[${ACCOUNT}]=$((${TOTAL[$ACCOUNT]}+${NCPU}))
     echo "  ${LINE} ncpu=$NCPU"
   fi
 done
+
